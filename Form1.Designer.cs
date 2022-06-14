3839cc0a2848672f5fdd41ddd42b91e4ce789ff0
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-06-14 10:15:01 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace webPreglednik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.naprijed = new System.Windows.Forms.Button();
+            this.nazad = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(150, 11);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(546, 37);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.naprijed);
+            this.panel2.Controls.Add(this.nazad);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 52);
+            this.panel2.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackgroundImage = global::webPreglednik.Properties.Resources.search_icon;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Location = new System.Drawing.Point(748, 11);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(40, 38);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackgroundImage = global::webPreglednik.Properties.Resources.favorite_35;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(702, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 39);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // naprijed
+            // 
+            this.naprijed.BackgroundImage = global::webPreglednik.Properties.Resources.naprijed;
+            this.naprijed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.naprijed.Location = new System.Drawing.Point(58, 11);
+            this.naprijed.Name = "naprijed";
+            this.naprijed.Size = new System.Drawing.Size(40, 39);
+            this.naprijed.TabIndex = 0;
+            this.naprijed.UseVisualStyleBackColor = true;
+            this.naprijed.Click += new System.EventHandler(this.naprijed_Click);
+            // 
+            // nazad
+            // 
+            this.nazad.BackgroundImage = global::webPreglednik.Properties.Resources.nazad;
+            this.nazad.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.nazad.Location = new System.Drawing.Point(12, 10);
+            this.nazad.Name = "nazad";
+            this.nazad.Size = new System.Drawing.Size(40, 39);
+            this.nazad.TabIndex = 1;
+            this.nazad.UseVisualStyleBackColor = true;
+            this.nazad.Click += new System.EventHandler(this.nazad_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::webPreglednik.Properties.Resources.refresh;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(104, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 38);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Google Chrome";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button naprijed;
+        private System.Windows.Forms.Button nazad;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using EasyTabs;
+
+namespace webPreglednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitChromium();
+        }
+        public ChromiumWebBrowser c;
+        public void InitChromium()
+        {
+            if (!Cef.IsInitialized)
+            {
+                Cef.Initialize(new CefSettings());
+            }
+
+            c = new ChromiumWebBrowser("https://www.google.com/");
+            c.AddressChanged += C_AddressChanged;
+            c.TitleChanged += C_TitleChanged;
+            this.panel1.Controls.Add(c);
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void C_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Text = e.Title;
+        }
+
+        private void C_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            textBox1.Text = e.Address.ToString();
+        }
+
+        private void nazad_Click(object sender, EventArgs e)
+        {
+            c.Back();
+        }
+
+        private void naprijed_Click(object sender, EventArgs e)
+        {
+            c.Forward();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            c.Load(textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            c.Load("https://chrome://bookmarks/");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
